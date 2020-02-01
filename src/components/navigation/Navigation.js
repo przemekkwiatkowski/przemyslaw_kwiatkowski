9e0c78cc23dc57fa08304a36ad88a0c3f432cf64
Przemek Kwiatkowski <appkwiatkowski@gmail.com>
2020-02-01 17:25:19 +0100

    Add navigation component

---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import classNames from 'classnames';
+
+const Navigation = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onToggle = () => setIsOpen(!isOpen);
+
+  const handleClick= () => onToggle();
+
+  return (
+    <nav className="navbar navbar-expand-sm navbar-dark bg-primary mb-3">
+      <Link to="/" className="navbar-brand">
+        Sonalake Task
+      </Link>
+
+      <button
+        type="button"
+        onClick={handleClick}
+        className={classNames('navbar-toggler', {
+          collapsed: !isOpen
+        })}
+        aria-expanded={isOpen}
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+
+      <div
+        className={classNames('collapse', 'navbar-collapse', {
+          show: isOpen
+        })}
+      >
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <Link to="/" className="nav-link">
+              List View
+              <span className="sr-only">(current)</span>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
