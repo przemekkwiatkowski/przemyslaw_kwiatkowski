9456ed3f285066a734d05384283c3fa13d4559b5
Przemek Kwiatkowski <appkwiatkowski@gmail.com>
2020-02-01 17:58:30 +0100

    Update test

--- src/app.test.js
+++ src/app.test.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { MemoryRouter } from 'react-router-dom';
-import AppComponent from './app.component';
+import App from './app.component';
 
 describe('<App />', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(
       <MemoryRouter>
-        <AppComponent />
+        <App />
       </MemoryRouter>,
       div
     );
