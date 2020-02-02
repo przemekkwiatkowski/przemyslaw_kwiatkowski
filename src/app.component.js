d83d7b1f3ed3b8773eac607c2f319401731483dd
Przemek Kwiatkowski <appkwiatkowski@gmail.com>
2020-02-02 13:53:09 +0100

    Add edit character route

--- src/app.component.js
+++ src/app.component.js
@@ -5,6 +5,7 @@ import { ROUTES } from './app.constants';
 import Navigation from './components/navigation/navigation.component';
 import ListView from './routes/listView/listView.component';
 import AddCharacter from './routes/addCharacter/addCharacter.component';
+import EditCharacter from './routes/editCharacter/editCharacter.component';
 
 class AppComponent extends Component {
   render() {
@@ -13,7 +14,8 @@ class AppComponent extends Component {
         <Navigation />
         <div className="container">
           <Route exact path={ROUTES.home} component={ListView} />
-          <Route exact path={ROUTES.addCharacter} component={AddCharacter} />
+          <Route path={ROUTES.addCharacter} component={AddCharacter} />
+          <Route path={`${ROUTES.editCharacter}/:id`} component={EditCharacter} />
         </div>
       </div>
     );
