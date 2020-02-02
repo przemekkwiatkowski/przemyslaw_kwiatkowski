33e5d9652377a88ec7ab6db94db08cea9cb5fd19
Przemek Kwiatkowski <appkwiatkowski@gmail.com>
2020-02-02 13:14:23 +0100

    Add possibility to delete character

--- src/components/dataTable/tableRow/tableRow.component.js
+++ src/components/dataTable/tableRow/tableRow.component.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { checkResponse, deleteData, url } from '../../../utils/api';
 
 const TableRow = ({ data:{ id, name, species, gender, homeworld } }) => {
+  const [removeError, setRemoveError] = useState(false);
+
+  const handleRemove = async () => {
+    setRemoveError(false);
+
+    try {
+      const response = await deleteData(url.characters, id);
+      checkResponse(response);
+      //updateData
+    } catch (error) {
+      console.error(error);
+      setRemoveError(true);
+    }
+  };
+
   return (
     <tr>
       <th scope="row">{id}</th>
@@ -17,8 +33,9 @@ const TableRow = ({ data:{ id, name, species, gender, homeworld } }) => {
           <button type="button" className="btn btn-secondary">
             <i className="fa fa-pencil" aria-hidden="true" /> Edit
           </button>
-          <button type="button" className="btn btn-danger">
-            <i className="fa fa-trash-o" aria-hidden="true" /> Remove
+          <button type="button" className="btn btn-danger" onClick={handleRemove}>
+            <i className="fa fa-trash-o" aria-hidden="true" />
+            Remove
           </button>
         </div>
       </td>
